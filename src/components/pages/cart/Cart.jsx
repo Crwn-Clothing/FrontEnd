5ad70f47f3fb8305d7023340b9c5da1638dc9f51
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-01-16 11:52:13 -0500

    Cart component complete, along with correct routing

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Grid, makeStyles, Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+// import cartActions from "../../../../actions/pages/cart";
 
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1
+	},
+	image: {
+		margin: "20px",
+		paddingTop: "50px",
+
+		width: "300px",
+		height: "300px",
+		backgroundSize: "cover"
+	},
+	name: {
+		marginLeft: "20px",
+		textAlign: "center",
+		width: "300px"
+	},
+	price: {
+		marginLeft: "20px",
+		textAlign: "center",
+		width: "300px"
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: "center",
+		color: theme.palette.text.secondary
+	}
+}));
 const CartPage = props => {
+	const renderGrids = () => {
+		return (
+			<Grid spacing={3} item xs={2}>
+				<img
+					className={classes.image}
+					src={props.cartItem.imageUrl}
+					alt={props.cartItem.name}
+				/>
+				<h4 className={classes.name}>{props.cartItem.name}</h4>
+				<p className={classes.price}>${props.cartItem.price_cents}</p>
+			</Grid>
+		);
+	};
+	const classes = useStyles();
 	return (
-		<div>
-			<h1>Cart</h1>
+		<div className={classes.root}>
+			<Grid container spacing={3}>
+				{renderGrids()}
+			</Grid>
 		</div>
 	);
 };
--- src/components/pages/cart/CartContainer.jsx
+++ src/components/pages/cart/CartContainer.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import CartPage from "./Cart";
+import { useSelector, useDispatch } from "react-redux";
+import cartActions from "../../../actions/pages/cart";
+import { Button } from "@material-ui/core";
 import "./Cart.scss";
 
-import CartPage from "./Cart";
-
 const CartContainer = props => {
+	const userCart = useSelector(state => state.cart.userCart);
+	const isLoggedIn = useSelector(state => state.userLogin.isLoggedIn);
+
+	const renderCartItem = () => {
+		return userCart.map(cartItem => {
+			return <CartPage key={cartItem.id} cartItem={cartItem} />;
+		});
+	};
+
+	const redirect = () => {
+		props.history.push("/checkout");
+	};
 	return (
 		<div>
-			<CartPage />
+			{renderCartItem()}
+			{isLoggedIn ? (
+				<Button
+					onClick={redirect}
+					variant="outlined"
+					type="submit"
+					style={{
+						margin: "20px auto",
+						background: "black",
+						width: "80%",
+						color: "whitesmoke"
+					}}
+				>
+					Proceed To Checkout
+				</Button>
+			) : null}
 		</div>
 	);
 };
